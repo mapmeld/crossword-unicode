3e9a1da4016c6e36e4aaa56ab1e28b5b1c8e3da5
Nick Doiron <ndoiron@mapmeld.com>
2016-01-20 07:09:02 +0545

    continue crossword bugs

--- scripts/crossword.js
+++ scripts/crossword.js
@@ -6,6 +6,9 @@ $(function() {
   ctx.font = '18px sans-serif';
 
   var downFirst = true;
+  if (Math.random() > 0.5) {
+    downFirst = false;
+  }
   clearCanvas(true);
 
   function clearCanvas(andPuzzle) {
@@ -24,7 +27,10 @@ $(function() {
   }
 
   $("#add-clue button").click(function() {
-    var word = $("#add-clue input").val();
+    var word = $("#add-clue input").val().split(/\s/)[0];
+    if (!word.length) {
+      return;
+    }
     word = word.split("");
     $("#add-clue input").val("");
 
@@ -45,7 +51,7 @@ $(function() {
         }
       }
     }
-    // fit on puzzle
+
     downFirst = !downFirst;
   });
 
@@ -67,15 +73,26 @@ $(function() {
   }
 
   function loop(startIn) {
-    for (var col = 2; col < 13; col++) {
-      for (var row = 2; row < 13; row++) {
+    /*
+    for (var r = 0; r < 50; r++) {
+      var row = Math.floor(Math.random() * 7);
+      var col = Math.floor(Math.random() * 7);
+      var madeFit = startIn(row + 5, col + 5);
+      if (madeFit) {
+        return true;
+      }
+    }
+    */
+
+    for (var col = 3; col < 12; col++) {
+      for (var row = 3; row < 12; row++) {
         var madeFit = startIn(row, col);
         if (madeFit) {
           return true;
         }
       }
       // still here
-      for (var col2 in [0, 1, 13, 14]) {
+      for (var col2 in [0, 1, 3, 12, 13, 14]) {
         var madeFit = startIn(row, col2);
         if (madeFit) {
           return true;
@@ -84,7 +101,7 @@ $(function() {
     }
 
     // still here
-    for (var col in [0, 1, 13, 14]) {
+    for (var col in [0, 1, 2, 12, 13, 14]) {
       for (var row = 0; row < 15; row++) {
         var madeFit = startIn(row, col);
         if (madeFit) {
@@ -97,7 +114,7 @@ $(function() {
 
   function fitDown(word) {
     function startIn(row, col) {
-      if (row + word.length > 15) {
+      if (row * 1 + word.length > 15) {
         // word is too long
         return false;
       }
@@ -109,8 +126,8 @@ $(function() {
         // letter above beginning
         return false;
       }
-      if ((col > 0 && grid[col - 1][row] && grid[col - 1][row].end) || (col < 14 && grid[col + 1][row] && grid[col + 1][row].label)) {
-        // letter left or right of beginning, which is start / end
+      if (row * 1 + word.length < 15 && grid[col][row * 1 + word.length]) {
+        // letter after end
         return false;
       }
 
@@ -121,12 +138,16 @@ $(function() {
         if (grid[x][y] && grid[x][y].letter !== letter) {
           return false;
         }
+        if ((x > 0 && grid[x - 1][y] && grid[x - 1][y].end) || (x < 14 && grid[x * 1 + 1][y] && grid[x * 1 + 1][y].label)) {
+          // letter left or right of box, which is start / end
+          return false;
+        }
       }
 
       // confirmed
       var clueNum = myanmarNumbers($("#clues li").length + 1, 'my');
       var clue = makeClue(clueNum, word.join(''));
-      $("#down ul").append(clue);
+      $("#down .list").append(clue);
 
       for (var char = 0; char < word.length; char++) {
         var y = row * 1 + char * 1;
@@ -147,7 +168,7 @@ $(function() {
 
   function fitAcross(word) {
     function startIn(row, col) {
-      if (col + word.length > 15) {
+      if (col * 1 + word.length > 15) {
         // word is too long
         return false;
       }
@@ -159,8 +180,8 @@ $(function() {
         // letter left of beginning
         return false;
       }
-      if ((row > 0 && grid[col][row - 1] && grid[col][row - 1].end) || (row < 14 && grid[col][row + 1] && grid[col][row + 1].label)) {
-        // letter above or below beginning, which is start / end
+      if (col * 1 + word.length < 15 && grid[col * 1 + word.length][row]) {
+        // letter right of ends
         return false;
       }
 
@@ -171,12 +192,16 @@ $(function() {
         if (grid[x][y] && (grid[x][y].letter !== letter)) {
           return false;
         }
+        if ((y > 0 && grid[x][y - 1] && grid[x][y - 1].end) || (y < 14 && grid[x][y * 1 + 1] && grid[x][y * 1 + 1].label)) {
+          // letter above or below beginning, which is start / end
+          return false;
+        }
       }
 
       // confirmed
       var clueNum = myanmarNumbers($("#clues li").length + 1, 'my');
       var clue = makeClue(clueNum, word.join(''));
-      $("#across ul").append(clue);
+      $("#across .list").append(clue);
 
       for (var char = 0; char < word.length; char++) {
         var x = col * 1 + char * 1;
